00883da90c26adefd2a283bd2aaf0fa78a6d5f09
Danilo Hoffmann <dhhyi@aol.com>
2021-10-12 12:45:54 +0200

    basic interpolate functionality

--- src/app/edit-dialog.component.ts
+++ src/app/edit-dialog.component.ts
@@ -1,5 +1,10 @@
 import { Component, Inject } from "@angular/core";
+import { FormControl } from "@angular/forms";
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
+import { TranslateCompiler, TranslateParser } from "@ngx-translate/core";
+import { Observable } from "rxjs";
+import { map, startWith } from "rxjs/operators";
 
 import { LocalizationWithBaseType } from "../services/localizations.service";
 
@@ -13,12 +18,17 @@ import { LocalizationWithBaseType } from "../services/localizations.service";
         cdkFocusInitial
         id="translation"
         cols="30"
-        rows="10"
-        [(ngModel)]="translation"
+        rows="5"
+        [formControl]="translation"
       ></textarea>
+      <div [innerHTML]="interpolation$ | async"></div>
     </div>
     <div mat-dialog-actions>
-      <button mat-raised-button color="primary" [matDialogClose]="translation">
+      <button
+        mat-raised-button
+        color="primary"
+        [matDialogClose]="translation.value"
+      >
         OK
       </button>
       <button mat-button [matDialogClose]="undefined">Cancel</button>
@@ -35,16 +45,43 @@ import { LocalizationWithBaseType } from "../services/localizations.service";
 export class EditDialogComponent {
   key: string;
   base: string;
-  translation: string;
+  translation = new FormControl("");
+
+  interpolation$: Observable<SafeHtml>;
 
   constructor(
     @Inject(MAT_DIALOG_DATA)
     data: {
       element: LocalizationWithBaseType;
-    }
+    },
+    private compiler: TranslateCompiler,
+    private parser: TranslateParser,
+    sanitizer: DomSanitizer
   ) {
     this.key = data.element.key;
     this.base = data.element.base;
-    this.translation = data.element.tr;
+    this.translation.setValue(data.element.tr);
+    this.interpolation$ = this.translation.valueChanges.pipe(
+      startWith(data.element.tr),
+      map((tr) =>
+        this.renderTranslation(tr, {
+          "0": "#1",
+          "1": "#2",
+          "2": "#3",
+        })
+      ),
+      map((interpolated) => sanitizer.bypassSecurityTrustHtml(interpolated))
+    );
+  }
+
+  private renderTranslation(tr: string, params: object): string {
+    try {
+      return this.parser.interpolate(
+        this.compiler.compile(tr, undefined),
+        params
+      );
+    } catch (err) {
+      return err instanceof Error ? err.message : err.toString();
+    }
   }
 }
