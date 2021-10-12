3c5ffd8e7fead7efa70b1c0cb5b4b0a722d91a1d
Danilo Hoffmann <dhhyi@aol.com>
2021-10-12 12:45:49 +0200

    guard backend

--- server.mjs
+++ server.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import { join, dirname } from "path";
+import { join } from "path";
 import { existsSync } from "fs";
 import { Low, JSONFile } from "lowdb";
 import cors from "cors";
@@ -28,54 +28,9 @@ app.get(/.*(html|css|js|ico)$/, express.static("dist"));
 
 // </ANGULAR>
 
-// <DB>
+// <OPEN-DB>
 
 app.use(cors());
-
-app.use(express.json({ limit: "50mb", extended: true }));
-app.use(express.text());
-
-function wrap(res, data) {
-  if (data) {
-    res.send(data);
-  } else {
-    res.sendStatus(404);
-  }
-}
-
-function assertFormat(req, format, res) {
-  if (req.get("Content-Type") !== format) {
-    res.status(400).send(`Only Content-Type "${format}" is allowed here!`);
-    return false;
-  }
-  return true;
-}
-
-function guard(req, res) {
-  if (req.get("Authorization") !== "super-safe-password") {
-    res.sendStatus(401);
-    return false;
-  }
-  return true;
-}
-
-app.get("/localizations", (_, res) => {
-  res.send(db.data);
-});
-
-app.post("/localizations", async (req, res) => {
-  if (assertFormat(req, "application/json", res) && guard(req, res)) {
-    db.data = req.body;
-    await db.write();
-    res.sendStatus(204);
-  }
-});
-
-app.get("/localizations/config", (req, res) => {
-  if (guard(req, res)) {
-    res.send(db.data.config || {});
-  }
-});
 
 function filterBlockedKeys(obj) {
   const blocked = db.data.config["block-keys"] || [];
@@ -85,52 +40,109 @@ function filterBlockedKeys(obj) {
   return obj;
 }
 
-app.get("/localizations/:locale", (req, res) => {
-  let lang;
-  if (req.query.exact !== "true") {
-    if (db.data.config?.block === "true") {
-      res.send({});
-    } else {
-      lang = req.params.locale.replace(".json", "");
-      const regex = /([a-z]{2})_[A-Z]{2}/;
-      if (regex.test(lang)) {
-        lang = regex.exec(lang)[1];
-      }
-      res.send(filterBlockedKeys(db.data[lang]) || {});
-    }
+app.get("/localizations/:locale", (req, res, next) => {
+  if (req.params.locale === "config") {
+    next();
   } else {
-    res.send(filterBlockedKeys(db.data[req.params.locale]) || {});
+    let lang;
+    if (req.query.exact !== "true") {
+      if (db.data.config?.block === "true") {
+        res.send({});
+      } else {
+        lang = req.params.locale.replace(".json", "");
+        const regex = /([a-z]{2})_[A-Z]{2}/;
+        if (regex.test(lang)) {
+          lang = regex.exec(lang)[1];
+        }
+        return res.send(filterBlockedKeys(db.data[lang]) || {});
+      }
+    } else {
+      return res.send(filterBlockedKeys(db.data[req.params.locale]) || {});
+    }
   }
+});
+
+app.get("/localizations/:locale/:key", (req, res, next) => {
+  if (req.params.locale === "config") {
+    next();
+  } else {
+    const data = db.data[req.params.locale]?.[req.params.key];
+    if (data) {
+      return res.send(data);
+    }
+    return res.sendStatus(404);
+  }
+});
+
+// </OPEN-DB>
+
+// <DB>
+
+const API_PASSWORD = process.env.API_PASSWORD;
+
+app.use(express.json({ limit: "50mb", extended: true }));
+app.use(express.text());
+
+app.use((req, res, next) => {
+  if (API_PASSWORD && req.get("Authorization") !== API_PASSWORD) {
+    return res.sendStatus(401);
+  }
+  next();
+});
+
+function assertFormat(req, format, res) {
+  if (req.get("Content-Type") !== format) {
+    res.status(400).send(`Only Content-Type "${format}" is allowed here!`);
+    return false;
+  }
+  return true;
+}
+
+app.get("/localizations", (_, res) => {
+  return res.send(db.data);
+});
+
+app.post("/localizations", async (req, res) => {
+  if (assertFormat(req, "application/json", res)) {
+    db.data = req.body;
+    await db.write();
+    return res.sendStatus(204);
+  }
+});
+
+app.get("/localizations/config", (_, res) => {
+  return res.send(db.data.config || {});
+});
+
+app.get("/localizations/config/:key", (req, res) => {
+  const data = db.data.config?.[req.params.key];
+  return res.send(data);
 });
 
 app.post("/localizations/:locale", async (req, res) => {
-  if (assertFormat(req, "application/json", res) && guard(req, res)) {
+  if (assertFormat(req, "application/json", res)) {
     db.data[req.params.locale] = filterBlockedKeys(req.body);
     await db.write();
-    res.sendStatus(204);
+    return res.sendStatus(204);
   }
 });
 
-app.get("/localizations/:locale/:key", (req, res) => {
-  wrap(res, db.data[req.params.locale]?.[req.params.key]);
-});
-
 app.post("/localizations/:locale/:key", async (req, res) => {
-  if (assertFormat(req, "text/plain", res) && guard(req, res)) {
+  if (assertFormat(req, "text/plain", res)) {
     if (!db.data[req.params.locale]) {
       db.data[req.params.locale] = {};
     }
     console.log("set", req.params.locale, req.params.key, "=", req.body);
     db.data[req.params.locale][req.params.key] = req.body;
     await db.write();
-    res.sendStatus(204);
+    return res.sendStatus(204);
   }
 });
 
 // </DB>
 
-app.get("*", (_, res) => {
-  res.sendStatus(404);
+app.use((_, res) => {
+  return res.sendStatus(404);
 });
 
 app.listen(8000, () => {
