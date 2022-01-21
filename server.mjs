033775541c539decef56f2f73f1a1fbe2cc62c32
Danilo Hoffmann <dhhyi@aol.com>
2022-01-21 14:59:05 +0100

    own location for config

--- server.mjs
+++ server.mjs
@@ -41,19 +41,29 @@ function BlockList() {
 
 const blockList = BlockList();
 
-const DB_FILE_NAME = "db.json";
-const file = join(process.env.DB_LOCATION || process.cwd(), DB_FILE_NAME);
+function BackEnd(dbFileName) {
+  const file = join(process.env.DB_LOCATION || process.cwd(), dbFileName);
 
-console.log("storage location:", file);
+  console.log("storage location:", file);
 
-const adapter = new JSONFile(file);
-const db = new Low(adapter);
+  const adapter = new JSONFile(file);
+  const db = new Low(adapter);
 
-await db.read();
-db.data = db.data || {};
-if (!existsSync(DB_FILE_NAME)) {
-  await db.write();
+  return db;
 }
+
+async function init(db) {
+  await db.read();
+  db.data = db.data || {};
+  await db.write();
+  return db;
+}
+
+const localizations = await init(BackEnd("db.json"));
+
+Object.entries(localizations.data).forEach(([lang, translations]) => {
+  console.log("loaded", lang, "-", Object.keys(translations).length, "keys");
+});
 
 const app = express();
 
@@ -83,13 +93,13 @@ app.get("/localizations/:locale", (req, res, next) => {
       } else {
         lang = req.params.locale.replace(".json", "");
         const regex = /([a-z]{2})_[A-Z]{2}/;
-        if (!db.data[lang] && regex.test(lang)) {
+        if (!localizations.data[lang] && regex.test(lang)) {
           lang = regex.exec(lang)[1];
         }
-        return res.send(db.data[lang] || {});
+        return res.send(localizations.data[lang] || {});
       }
     } else {
-      return res.send(db.data[req.params.locale] || {});
+      return res.send(localizations.data[req.params.locale] || {});
     }
   }
 });
@@ -98,7 +108,7 @@ app.get("/localizations/:locale/:key", (req, res, next) => {
   if (req.params.locale === "config") {
     next();
   } else {
-    const data = db.data[req.params.locale]?.[req.params.key];
+    const data = localizations.data[req.params.locale]?.[req.params.key];
     if (data) {
       return res.send(data);
     }
@@ -107,8 +117,6 @@ app.get("/localizations/:locale/:key", (req, res, next) => {
 });
 
 // </OPEN-DB>
-
-// <DB>
 
 const API_PASSWORD = process.env.API_PASSWORD;
 
@@ -130,66 +138,18 @@ function assertFormat(req, format, res) {
   return true;
 }
 
+// <DB>
+
 app.get("/localizations", (_, res) => {
-  return res.send(db.data);
+  return res.send(localizations.data);
 });
 
 app.post("/localizations", async (req, res) => {
   if (assertFormat(req, "application/json", res)) {
-    db.data = req.body;
-    await db.write();
+    localizations.data = req.body;
+    await localizations.write();
     return res.sendStatus(204);
   }
-});
-
-const getConfig = () => {
-  return {
-    ...(db.data.config || {}),
-    translateAvailable: !!process.env.GOOGLE_API_KEY,
-  };
-};
-
-const setConfigValue = async (key, value) => {
-  const config = getConfig();
-  config[key] = value;
-  db.data.config = _.omit(config, "translateAvailable");
-  await db.write();
-};
-
-app.get("/localizations/config", (_, res) => {
-  return res.send(getConfig());
-});
-
-app.get("/localizations/config/block", (req, res) => {
-  return res.send(blockList.includes(req.ip));
-});
-
-app.put("/localizations/config/block", (req, res) => {
-  blockList.add(req.ip);
-  return res.sendStatus(204);
-});
-
-app.delete("/localizations/config/block", (req, res) => {
-  blockList.remove(req.ip);
-  return res.sendStatus(204);
-});
-
-app.get("/localizations/config/:key", (req, res) => {
-  const data = getConfig()[req.params.key];
-  return res.send(data);
-});
-
-app.post("/localizations/config/:key", async (req, res) => {
-  let value = req.body;
-  if (typeof value === "string") {
-    if ("true" === value?.toLowerCase()) {
-      value = true;
-    } else if ("false" === value?.toLowerCase()) {
-      value = false;
-    }
-  }
-  await setConfigValue(req.params.key, value);
-  return res.sendStatus(204);
 });
 
 app.post("/localizations/import/:locale", async (req, res) => {
@@ -236,63 +196,127 @@ app.post("/localizations/import/:locale", async (req, res) => {
         .send("Could not parse any data in the CSV or JSON content");
     }
 
-    if (!db.data[req.params.locale]) {
-      db.data[req.params.locale] = {};
+    if (!localizations.data[req.params.locale]) {
+      localizations.data[req.params.locale] = {};
     }
 
     switch (req.query.type) {
       case "replace":
-        db.data[req.params.locale] = data;
+        localizations.data[req.params.locale] = data;
         break;
 
       case "overwrite":
-        db.data[req.params.locale] = { ...db.data[req.params.locale], ...data };
+        localizations.data[req.params.locale] = {
+          ...localizations.data[req.params.locale],
+          ...data,
+        };
         break;
 
       case "add":
-        db.data[req.params.locale] = { ...data, ...db.data[req.params.locale] };
+        localizations.data[req.params.locale] = {
+          ...data,
+          ...localizations.data[req.params.locale],
+        };
         break;
 
       default:
         return res.sendStatus(400);
     }
 
-    await db.write();
+    await localizations.write();
     return res.sendStatus(204);
   }
 });
 
 app.post("/localizations/:locale", async (req, res) => {
   if (assertFormat(req, "application/json", res)) {
-    db.data[req.params.locale] = req.body;
-    await db.write();
+    localizations.data[req.params.locale] = req.body;
+    await localizations.write();
     return res.sendStatus(204);
   }
 });
 
 app.post("/localizations/:locale/:key", async (req, res) => {
   if (assertFormat(req, "text/plain", res)) {
-    if (!db.data[req.params.locale]) {
-      db.data[req.params.locale] = {};
+    if (!localizations.data[req.params.locale]) {
+      localizations.data[req.params.locale] = {};
     }
     console.log("set", req.params.locale, req.params.key, "=", req.body);
-    db.data[req.params.locale][req.params.key] = req.body;
-    await db.write();
+    localizations.data[req.params.locale][req.params.key] = req.body;
+    await localizations.write();
     return res.sendStatus(204);
   }
 });
 
 app.delete("/localizations/:locale/:key", async (req, res) => {
-  if (!db.data[req.params.locale]) {
-    db.data[req.params.locale] = {};
+  if (!localizations.data[req.params.locale]) {
+    localizations.data[req.params.locale] = {};
   }
   console.log("delete", req.params.locale, req.params.key);
-  db.data[req.params.locale][req.params.key] = undefined;
-  await db.write();
+  localizations.data[req.params.locale][req.params.key] = undefined;
+  await localizations.write();
   return res.sendStatus(204);
 });
 
 // </DB>
+
+// <CONFIG>
+
+const config = await init(BackEnd("config.json"));
+
+const getConfig = () => {
+  return {
+    ...(config.data || {}),
+    translateAvailable: !!process.env.GOOGLE_API_KEY,
+  };
+};
+
+console.log("loaded config", getConfig());
+
+const setConfigValue = async (key, value) => {
+  const config = getConfig();
+  config[key] = value;
+  config.data = _.omit(config, "translateAvailable");
+  await config.write();
+};
+
+app.get("/localizations/config", (_, res) => {
+  return res.send(getConfig());
+});
+
+app.get("/localizations/config/block", (req, res) => {
+  return res.send(blockList.includes(req.ip));
+});
+
+app.put("/localizations/config/block", (req, res) => {
+  blockList.add(req.ip);
+  return res.sendStatus(204);
+});
+
+app.delete("/localizations/config/block", (req, res) => {
+  blockList.remove(req.ip);
+  return res.sendStatus(204);
+});
+
+app.get("/localizations/config/:key", (req, res) => {
+  const data = getConfig()[req.params.key];
+  return res.send(data);
+});
+
+app.post("/localizations/config/:key", async (req, res) => {
+  let value = req.body;
+  if (typeof value === "string") {
+    if ("true" === value?.toLowerCase()) {
+      value = true;
+    } else if ("false" === value?.toLowerCase()) {
+      value = false;
+    }
+  }
+  await setConfigValue(req.params.key, value);
+  return res.sendStatus(204);
+});
+
+// </CONFIG>
 
 // <TRANSLATE>
 
